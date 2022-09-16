226892fddc9e624b14350f6fc0ac49e89e8c02fe
dilip <imdilip.patil666@gmail.com>
2022-09-16 23:26:47 +0530

    5th Commit

--- product-webapp/product-webapp/server/server.js
+++ product-webapp/product-webapp/server/server.js
@@ -47,4 +47,4 @@ io.on("connection", (socket) => {
 	});
 })
 
-server.listen(3000, () => console.log("server is running on port 5000"))
+server.listen(5000, () => console.log("server is running on port 5000"))
